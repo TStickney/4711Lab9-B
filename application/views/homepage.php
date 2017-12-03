f5105859817ff8ee564f4a42a4151806a7d49bf8
TStickney <tstick56@hotmail.com>
2017-12-03 12:41:06 -0800

    Created a backend server web-app.

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,11 +1,1 @@
-<div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
-
-<table class="table">
-{display_tasks}
-<tr>
-    <td>{id}</td>
-    <td>{task}</td>
-    <td>{priority}</td>
-</tr>
-{/display_tasks}
-</table>
+<h1>This is a server web-app.</h1>
